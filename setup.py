793273b1c1c7842f7d27d06e4b57eb981f1e2703
Rishi Verma <rv030548@gmail.com>
2026-06-10 13:22:58 +0530

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='CustomerChurnPrediction'
+    version='0.0.1',
+    author='Rishi',
+    author_email='rv030548@.com'
+    packages=find_packages()
+    
+)
