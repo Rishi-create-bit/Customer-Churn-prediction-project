5366a879549f9421e8186a8d0f3cb1aa9dbe824d
Rishi Verma <rv030548@gmail.com>
2026-06-13 14:02:11 +0530

    logging and Exception

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import logging 
+import os 
+from datetime import datetime
+LOG_FILE=f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.long"
+log_path =os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(log_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s]%(lineno)d%(name)s_%(levelname)s_%(message)s",
+    level=loging.INFO,
+)
+
+if __name__=="__main__":
+    logging.info("logging has started ")
