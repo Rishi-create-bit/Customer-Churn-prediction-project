f99522ab6eef29518ce0d8d9fab8ff04cf2bb4b0
Rishi Verma <rv030548@gmail.com>
2026-06-19 13:53:44 +0530

    Complete data transformation pipeline

---
+++ component/data_transformation.py
@@ -0,0 +1,148 @@
+import os
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from  sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging 
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path: str=os.path.join('artifacts',"preprocessor.pkl")
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            numerical_columns=[
+               "MonthlyCharges",
+               "TotalCharges",
+               "tenure",
+               ]
+            categorical_columns=[  
+               "gender",
+               "SeniorCitizen",
+               "Partner",
+               "Dependents",
+                "PhoneService",
+               "MultipleLines",
+                "InternetService",
+                "OnlineSecurity",
+                "OnlineBackup",
+                "DeviceProtection",
+                 "TechSupport",
+                 "StreamingTV",
+                "StreamingMovies",
+                "Contract",
+               "PaperlessBilling",
+                "PaymentMethod",
+]
+
+            num_pipeline = Pipeline(steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler()),
+                ]
+                
+            )
+
+            cat_pipeline=Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler",StandardScaler(with_mean=False))
+                ]
+            )
+            logging.info("numerical columns standard scaling completed")
+            logging.info("categorical columns encoding completed")
+
+            preprocessor=ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns),
+                    ("cat_pipeline",cat_pipeline,categorical_columns)
+                    
+                ]
+            )
+            return preprocessor
+        
+
+
+        except Exception as e :
+            raise CustomException(e,sys)
+            
+
+    def initiate_data_transformation(self,train_path, test_path): 
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+            # Convert TotalCharges to numeric
+            train_df["TotalCharges"] = pd.to_numeric(train_df["TotalCharges"], errors="coerce")
+            test_df["TotalCharges"] = pd.to_numeric(test_df["TotalCharges"], errors="coerce")  
+           
+
+            logging.info("the train and test data completed")
+            logging.info("obtaining preprocessing object")
+       
+            preprocessing_obj=self.get_data_transformer_object()
+
+            target_columns_name="Churn"
+        
+
+            input_feature_train_df = train_df.drop(columns=[target_columns_name, "customerID"])
+            target_feature_train_df=train_df[target_columns_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_columns_name,"customerID"])
+            target_feature_test_df=test_df[target_columns_name]
+
+            logging.info(
+            f"applying preprocessing object on training dataframe and testing datframe"
+            )
+            
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            train_arr=np.c_[
+            input_feature_train_arr,np.array(target_feature_train_df)
+         ]
+            test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+            logging.info(f"saved preprocessing object")
+
+
+            save_object(file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                    obj=preprocessing_obj)
+        
+            return(
+            train_arr,
+            test_arr,
+            self.data_transformation_config.preprocessor_obj_file_path,
+         )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+if __name__ == "__main__":
+    from component.data_ingestion import DataIngestion
+
+    ingestion = DataIngestion()
+
+    train_path, test_path = ingestion.initiate_data_ingestion()
+
+    transformation = DataTransformation()
+
+    train_arr, test_arr, preprocessor_path = transformation.initiate_data_transformation(
+        train_path,
+        test_path
+    )
+
+    print(train_arr.shape)
+    print(test_arr.shape)
+    print(preprocessor_path)
